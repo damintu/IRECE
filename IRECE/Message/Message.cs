97de08d269e9965ea7a33aedd428f8d412fb2e5d
unknown <bouchez@PC-LAB1-18.ntuser.ece.fr>
2015-03-16 15:17:43 +0100

    Adds basic json (de)serialization to the Message class

--- IRECE/Message/Message.cs
+++ IRECE/Message/Message.cs
@@ -3,17 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
 
 namespace IRECE.Message
 {
-    public abstract class Message
+    [DataContract]
+    public class Message
     {
-        public string Type { get { return ""; } }
+        [DataMember]
+        public string Command { get; set; }
+        [DataMember]
+        public string Channel { get; set; }
+        [DataMember]
         public string Text { get; set; }
 
-        public String ToString()
+        public override string ToString()
         {
-            return "";
+            string str;
+            using (MemoryStream s = new MemoryStream())
+            using (StreamReader sr = new StreamReader(s))
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Message));
+                serializer.WriteObject(s, this);
+                s.Position = 0;
+                str = sr.ReadToEnd();
+            }
+            return str;
+        }
+
+        public static Message Deserialize(string json)
+        {
+            Message m;
+            using (MemoryStream s = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json)))
+            using (StreamReader sr = new StreamReader(s))
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Message));
+                s.Position = 0;
+                m = (Message)serializer.ReadObject(s);
+            }
+            return m;
         }
     }
 }
